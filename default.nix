2439fa4468f58eea7f57a53075b8e12ffca625b9
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-11-25 21:34:51 +0100

    Add dunst

--- default.nix
+++ default.nix
@@ -26,64 +26,66 @@ in
   services.dunst = {
     enable = true;
     settings = ''
-      [global]
-          monitor = 0
-          follow = mouse
-          geometry = "300x60-15+46"
-          indicate_hidden = yes
-          shrink = yes
-          transparency = 0
-          notification_height = 0
-          separator_height = 2
-          padding = 8
-          horizontal_padding = 8
-          frame_width = 3
-          frame_color = "#000000"
-          separator_color = frame
-          sort = yes
-          idle_threshold = 120
-          font = Museo Sans 10
-          line_height = 0
-          markup = full
-          format = "<b>%s</b>\n%b"
-          alignment = left
-          show_age_threshold = 60
-          word_wrap = yes
-          ellipsize = middle
-          ignore_newline = no
-          stack_duplicates = true
-          hide_duplicate_count = false
-          show_indicators = yes
-          icon_position = left
-          max_icon_size = 32
-          sticky_history = yes
-          history_length = 20
-          title = Dunst
-          class = Dunst
-          startup_notification = false
-          verbosity = mesg
-          corner_radius = 8
-          mouse_left_click = close_current
-          mouse_middle_click = do_action
-          mouse_right_click = close_all
+      {
+        [global]
+            monitor = 0
+            follow = mouse
+            geometry = "300x60-15+46"
+            indicate_hidden = yes
+            shrink = yes
+            transparency = 0
+            notification_height = 0
+            separator_height = 2
+            padding = 8
+            horizontal_padding = 8
+            frame_width = 3
+            frame_color = "#000000"
+            separator_color = frame
+            sort = yes
+            idle_threshold = 120
+            font = Museo Sans 10
+            line_height = 0
+            markup = full
+            format = "<b>%s</b>\n%b"
+            alignment = left
+            show_age_threshold = 60
+            word_wrap = yes
+            ellipsize = middle
+            ignore_newline = no
+            stack_duplicates = true
+            hide_duplicate_count = false
+            show_indicators = yes
+            icon_position = left
+            max_icon_size = 32
+            sticky_history = yes
+            history_length = 20
+            title = Dunst
+            class = Dunst
+            startup_notification = false
+            verbosity = mesg
+            corner_radius = 8
+            mouse_left_click = close_current
+            mouse_middle_click = do_action
+            mouse_right_click = close_all
 
-      [urgency_low]
-          foreground = "#ffd5cd"
-          background = "#121212"
-          frame_color = "#181A20"
-          timeout = 10
+        [urgency_low]
+            foreground = "#ffd5cd"
+            background = "#121212"
+            frame_color = "#181A20"
+            timeout = 10
 
-      [urgency_normal]
-          background = "#121212"
-          foreground = "#ffd5cd"
-          frame_color = "#181A20"
-          timeout = 10
+        [urgency_normal]
+            background = "#121212"
+            foreground = "#ffd5cd"
+            frame_color = "#181A20"
+            timeout = 10
 
-      [urgency_critical]
-          background = "#121212"
-          foreground = "#ffd5cd" #FDF1DB #f5bfd2 #ff8585   #ffdfd3  #fec8d8      #FDF1DB #8fbcbb
-          frame_color = "#181A20"
-          timeout = 0
+        [urgency_critical]
+            background = "#121212"
+            foreground = "#ffd5cd" #FDF1DB #f5bfd2 #ff8585   #ffdfd3  #fec8d8      #FDF1DB #8fbcbb
+            frame_color = "#181A20"
+            timeout = 0
+      }
     '';
   };
 
